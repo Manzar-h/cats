f338499afad1564710079ef2630df27ce921fe19
Manzar <manzar.hussain@telus.com>
2022-11-23 17:54:23 -0500

    Asynchronous Return Values

---
+++ syncBreeds.js
@@ -0,0 +1,15 @@
+// data in memory
+const catBreeds = {
+  'Balinese': "Balinese are curious, outgoing, intelligent cats with excellent communication skills. They are known for their chatty personalities and are always eager to tell you their views on life, love, and what you’ve served them for dinner.",
+  'Bombay': ""
+};
+
+// synchronous function to fetch a cat breed
+const breedDetails = function(breed) {
+  // can simply return it (easy peezee, butter squeezy) ...
+  return catBreeds[breed];
+};
+
+// get the return value right away from the function
+const bombay = breedDetails('Bombay');
+console.log(bombay); //=> prints out the description for that breed
